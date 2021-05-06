0b286f50ef519813b7c897775fe704b280e97b85
hslavich <hernan.slavich@gmail.com>
2021-05-06 14:45:35 -0300

    Update galaxy tests

--- test/model/test_galaxy.py
+++ test/model/test_galaxy.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import MagicMock
 
 from model.Galaxy import Galaxy
 
@@ -7,4 +8,27 @@ class GalaxyTest(unittest.TestCase):
 
     def test_initial_state(self):
         galaxy = Galaxy()
+        self.assertEqual(galaxy.status, Galaxy.STATUS_DROUGHT)
+
+    def test_states(self):
+        galaxy = Galaxy()
+
+        positions = [(400, 500), (410, 1000), (420, 2000)]
+        galaxy.planet_positions = MagicMock(return_value=positions)
+        galaxy.check_status()
+        self.assertEqual(galaxy.status, Galaxy.STATUS_OPTIMAL)
+
+        positions = [(-211.31, -453.15), (819.15, 573.58), (-1931.85, -517.64)]
+        galaxy.planet_positions.return_value = positions
+        galaxy.check_status()
+        self.assertEqual(galaxy.status, Galaxy.STATUS_RAINY)
+
+        positions = [(211.31, -453.15), (819.15, 573.58), (1931.85, -517.64)]
+        galaxy.planet_positions.return_value = positions
+        galaxy.check_status()
+        self.assertEqual(galaxy.status, Galaxy.STATUS_QUIET)
+
+        positions = [(211.31, 0), (819.15, 0), (1931.85, 0)]
+        galaxy.planet_positions.return_value = positions
+        galaxy.check_status()
         self.assertEqual(galaxy.status, Galaxy.STATUS_DROUGHT)
