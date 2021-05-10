193f2e44c555c67c19669c4d5938c5d86afbe639
hslavich <hernan.slavich@gmail.com>
2021-05-10 16:08:25 -0300

    Cambio en impresion de resultados

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ if __name__ == '__main__':
         predictor.run(3650)
         print('Total períodos de sequía: %d' % len(predictor.drought_periods()))
         print('Total períodos de lluvia: %d' % len(predictor.rainy_periods()))
+        print('Día pico máximo de lluvia: %d' % predictor.max_rainy_day().max_area[0])
         print('Total períodos de condiciones óptimas: %d' % len(predictor.optimal_periods()))
-        print(predictor.max_rainy_day().max_area[0])
